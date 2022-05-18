b2838df06de2cc448c1438f7eb46a0a15e5b1c4b
Emily Christoph <EChristoph@ad.ninjatrader.com>
2022-05-18 13:03:25 -0600

    edit to guessing game

--- guessing-game/guessing-game/Program.cs
+++ guessing-game/guessing-game/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello, cool cats and kittens welcome Michael and Emily's guessing game.");
+Console.WriteLine("hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
 // Declare a variable (a number between 1-10) that is going to act as the number you will be guessing
 int num = 5;
