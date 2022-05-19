baa057cd05f73180b401b49360f27de14119df3b
Emily Christoph <EChristoph@ad.ninjatrader.com>
2022-05-19 12:37:28 -0600

    updated GuessingGame

--- guessing-game/guessing-game/Program.cs
+++ guessing-game/guessing-game/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
 // Declare a variable (a number between 1-10) that is going to act as the number you will be guessing
 int num = 5;
+Console.WriteLine("Guess a number between 1-10:");
+string userNum = Console.ReadLine();
+int userNumber = Convert.ToInt32(userNum);
+
+if (userNumber == num)
+{
+    Console.WriteLine("Wow, I'm surprised you got it right");
+}
