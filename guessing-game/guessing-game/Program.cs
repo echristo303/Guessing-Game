556a62b29ffdc631e6c37aedf4561391c9ca198a
MichaelGranzeier <granzeierpeter@gmail.com>
2022-05-19 15:04:35 -0400

    day 2 work added

--- guessing-game/guessing-game/Program.cs
+++ guessing-game/guessing-game/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
+Console.WriteLine("Hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
 // Declare a variable (a number between 1-10) that is going to act as the number you will be guessing
 int num = 5;
 Console.WriteLine("Guess a number between 1-10:");
@@ -10,3 +10,11 @@ if (userNumber == num)
 {
     Console.WriteLine("Wow, I'm surprised you got it right");
 }
+else if (userNumber >= 1 && userNumber <= 10) 
+    {
+        Console.WriteLine("No surprise you're WRONG!");
+    }
+else
+    {
+        Console.WriteLine("No, you can't do that!");
+    }
