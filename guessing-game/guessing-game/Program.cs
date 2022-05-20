ae8ab4be84a30f7fe372e7375490cf7fe3424813
Emily Christoph <EChristoph@ad.ninjatrader.com>
2022-05-20 11:46:27 -0600

    added random number

--- guessing-game/guessing-game/Program.cs
+++ guessing-game/guessing-game/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
 // Declare a variable (a number between 1-10) that is going to act as the number you will be guessing
-int num = 5;
+Random Random = new Random();
+int num = Random.Next(1, 10);
 int attemptNumber = 1;
 
 while (attemptNumber <= 3)
