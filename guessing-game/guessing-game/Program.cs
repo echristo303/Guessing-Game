3540c71277c9d07a494b575b3f3befb98a8a5fc1
Emily Christoph <EChristoph@ad.ninjatrader.com>
2022-05-20 11:28:11 -0600

    added loop

--- guessing-game/guessing-game/Program.cs
+++ guessing-game/guessing-game/Program.cs
@@ -2,19 +2,27 @@
 Console.WriteLine("Hello, cool cats and kittens welcome to Michael and Emily's guessing game.");
 // Declare a variable (a number between 1-10) that is going to act as the number you will be guessing
 int num = 5;
-Console.WriteLine("Guess a number between 1-10:");
-string userNum = Console.ReadLine();
-int userNumber = Convert.ToInt32(userNum);
 
-if (userNumber == num)
+int attemptNumber = 1;
+
+while (attemptNumber <= 3)
 {
-    Console.WriteLine("Wow, I'm surprised you got it right");
-}
-else if (userNumber >= 1 && userNumber <= 10) 
+    Console.WriteLine("Guess a number between 1-10:");
+    string userNum = Console.ReadLine();
+    int userNumber = Convert.ToInt32(userNum);
+
+    if (userNumber == num)
+    {
+        Console.WriteLine("Wow, I'm surprised you got it right");
+        break;
+    }
+    else if (userNumber >= 1 && userNumber <= 10)
     {
         Console.WriteLine("No surprise you're WRONG!");
     }
-else
+    else
     {
         Console.WriteLine("No, you can't do that!");
     }
+    attemptNumber++;
+}
